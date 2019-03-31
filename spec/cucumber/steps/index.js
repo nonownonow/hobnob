1638c3ddba462b7fb1dee3e012d5d31b270833e8
ryuilhwan <ih.ryu.dev@gmail.com>
2019-03-31 20:25:30 +0900

    WIP Implement Create User with Empty Payload

--- spec/cucumber/steps/index.js
+++ spec/cucumber/steps/index.js
@@ -1,25 +1,40 @@
 import { When, Then } from 'cucumber';
-import superagent from 'superagent'
-When('the client creates a POST request to /users', function (callback) {
-  request = superagent('POST', 'localhost:8080/users')
+import superagent from 'superagent';
+
+let request;
+let result;
+let error;
+
+When('the client creates a POST request to /users', () => {
+  request = superagent('POST', 'localhost:8080/users');
 });
 
-When('attaches a generic empty payload', function (callback) {
+When('attaches a generic empty payload', function () {
+  return undefined;
+});
+
+When('sends the request', (callback) => {
+  request
+    .then((response) => {
+      result = response.res;
+      callback();
+    })
+    .catch((errResponse) => {
+      error = errResponse.response;
+      callback();
+    });
+});
+
+Then('our API should respond with a 400 HTTP status code', (callback) => {
+  if(error.statusCode !== 400){
+    throw new Error()
+  }
+});
+
+Then('the payload of the response should be a JSON object', (callback) => {
   callback(null, 'pending');
 });
 
-When('sends the request', function (callback) {
-  callback(null, 'pending');
-});
-
-Then('our API should respond with a 400 HTTP status code', function (callback) {
-  callback(null, 'pending');
-});
-
-Then('the payload of the response should be a JSON object', function (callback) {
-  callback(null, 'pending');
-});
-
-Then('contains a message property which says "Payload should not be empty"', function (callback) {
+Then('contains a message property which says "Payload should not be empty"', (callback) => {
   callback(null, 'pending');
 });

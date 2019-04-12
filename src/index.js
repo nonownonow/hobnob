62ea983dc32baf47995b961267b47ecd27e5640c
ryuilhwan <ih.ryu.dev@gmail.com>
2019-04-12 22:57:07 +0900

    Check validity of email for Create User endpoint

--- src/index.js
+++ src/index.js
@@ -20,24 +20,7 @@ function checkEmptyPayload(req, res, next) {
   }
   next();
 }
-function checkMissingField(req, res, next) {
-  const requiredField = ['email', 'password'];
-  if (!requiredField.every(field => Object.hasOwnProperty.call(req.body, field))) {
-    res.status(400);
-    res.set('Content-Type', 'application/json');
-    res.json({
-      message: 'Payload must contain at least the email and password fields',
-    });
-  }
-  if (typeof req.body.email !== 'string' || typeof req.body.password !== 'string') {
-    res.status(400);
-    res.set('Content-Type', 'application/json');
-    res.json({
-      message: 'The email and password fields must be of type string',
-    });
-  }
-  next();
-}
+
 function checkContentTypeHeader(req, res, next) {
   if (!req.headers['content-type']) {
     res.status(400);
@@ -62,7 +45,31 @@ app.use(checkEmptyPayload);
 app.use(checkContentTypeHeader);
 app.use(checkContentTypeJSON);
 app.use(bodyparser.json({ limit: PAYLOAD_LIMIT }));
-app.post('/users', checkMissingField);
+app.post('/users', (req, res, next) => {
+  const requiredField = ['email', 'password'];
+  if (!requiredField.every(field => Object.hasOwnProperty.call(req.body, field))) {
+    res.status(400);
+    res.set('Content-Type', 'application/json');
+    res.json({
+      message: 'Payload must contain at least the email and password fields',
+    });
+  }
+  if (typeof req.body.email !== 'string' || typeof req.body.password !== 'string') {
+    res.status(400);
+    res.set('Content-Type', 'application/json');
+    res.json({
+      message: 'The email and password fields must be of type string',
+    });
+  }
+  if (!/^[\w.+]+@\w+\.\w+$/.test(req.body.email)) {
+    res.status(400);
+    res.set('Content-Type', 'application/json');
+    res.json({
+      message: 'The email field must be a valid email',
+    });
+  }
+  next();
+});
 
 app.post('/users', (req, res, next) => next());
 

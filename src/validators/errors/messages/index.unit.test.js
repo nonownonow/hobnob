b6af4f8ed991b37e54fafdb2e1349d3071fdbce4
ryuilhwan <ih.ryu.dev@gmail.com>
2019-05-29 06:03:21 +0900

    Complete unit tests for generateValidationErrorMessage

--- src/validators/errors/messages/index.unit.test.js
+++ src/validators/errors/messages/index.unit.test.js
@@ -14,4 +14,46 @@ describe('generateValidationErrorMessage', function () {
     const expectedErrorMessage = "The '.test.path.property' field is missing";
     assert.strictEqual(actualErrorMessage, expectedErrorMessage);
   });
+  it('should return the correct string when err.keyword is "type"', function () {
+    const errors = [{
+      keyword: 'type',
+      dataPath: '.test.path',
+      params: {
+        type: 'string',
+      },
+    }];
+    const actualErrorMessage = generateValidationErrorMessage(errors);
+    const expectedErrorMessage = "The '.test.path' field must be of type string";
+    assert.strictEqual(actualErrorMessage, expectedErrorMessage);
+  });
+  it('should return the correct string when err.keyword is "format"', function () {
+    const errors = [{
+      keyword: 'format',
+      dataPath: '.test.path',
+      params: {
+        format: 'path',
+      },
+    }];
+    const actualErrorMessage = generateValidationErrorMessage(errors);
+    const expectedErrorMessage = "The '.test.path' field must be a valid path";
+    assert.strictEqual(actualErrorMessage, expectedErrorMessage);
+  });
+  it('should return the correct string when err.keyword is "additionalProperties"', function () {
+    const errors = [{
+      keyword: 'additionalProperties',
+      dataPath: '.test.path',
+      params: {
+        additionalProperty: 'path',
+      },
+    }];
+    const actualErrorMessage = generateValidationErrorMessage(errors);
+    const expectedErrorMessage = "The '.test.path' object dose not support the field 'path'";
+    assert.strictEqual(actualErrorMessage, expectedErrorMessage);
+  });
+  it('should return the correct string when err.keyword is not recognized', function () {
+    const errors = [{}];
+    const actualErrorMessage = generateValidationErrorMessage(errors);
+    const expectedErrorMessage = "The object is invalid";
+    assert.strictEqual(actualErrorMessage, expectedErrorMessage);
+  });
 });

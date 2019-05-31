6d6f95f7aeea40bf2e6774f16165df0bb56bb749
ilhwan.ryu <ih.ryu.dev@gmail.com>
2019-05-31 21:26:04 +0900

    wip

--- src/middlewares/check-empty-playload/index.unit.test.js
+++ src/middlewares/check-empty-playload/index.unit.test.js
@@ -4,13 +4,14 @@ import isEqual from 'lodash.isequal';
 import { spy } from 'sinon';
 import { checkEmptyPayload } from './index';
 
-['POST', 'PATCH', 'PUT'].forEach(function (method) {
-  describe('checkEmptyPlayload', function () {
-    let req;
-    let res;
-    let next;
-    let cloneRes;
-    before(function () {
+describe('checkEmptyPlayload', function () {
+  let req;
+  let res;
+  let next;
+  let cloneRes;
+
+  describe(`When req.method is not one of POST, PATCH or PUT`, function () {
+    beforeEach(function () {
       req = {
         method: 'GET',
       };
@@ -19,13 +20,81 @@ import { checkEmptyPayload } from './index';
       cloneRes = clonedeep(res);
       checkEmptyPayload(req, res, next);
     });
-
-    describe(`When req.method is not one of ${method}`, function () {
-      it('should not modify res', function () {
-        assert(isEqual(res, cloneRes));
+    it('should not modify res', function () {
+      assert(isEqual(res, cloneRes));
+    });
+    it('should call next() once', function () {
+      assert(next.calledOnce);
+    });
+  });
+  ['POST', 'PATCH', 'PUT'].forEach(function (method) {
+    describe(`When req.method is ${method}`, function () {
+      describe('and the content-length header is not "0"', function () {
+        beforeEach(function () {
+          req = {
+            method,
+            headers: {
+              "content-length": '1',
+            },
+          };
+          res = {};
+          next = spy();
+          cloneRes = clonedeep(res);
+          checkEmptyPayload(req, res, next);
+        });
+        it('should not modify res', function () {
+          assert(isEqual(res, cloneRes));
+        });
+        it('should call next() once', function () {
+          assert(next.calledOnce);
+        });
       });
-      it('should call next() once', function () {
-        assert(next.calledOnce);
+      describe('and the content-length header is "0"', function () {
+        beforeEach(function () {
+          req = {
+            method,
+            headers: {
+              "content-length": '0',
+            },
+          };
+          res = {
+            status: spy(),
+            set: spy(),
+            json: spy(),
+          };
+          next = spy();
+          cloneRes = clonedeep(res);
+          checkEmptyPayload(req, res, next);
+        });
+        describe('should call res.status()', function () {
+          it('once', function () {
+            res.status.calledOnce;
+          });
+          it('with the argument 400', function () {
+            res.status.calledWithExactly(400);
+          });
+        });
+        describe('should call res.set()', function () {
+          it('once', function () {
+            res.set.calledOnce;
+          });
+          it('with the arguments "Content-Type" and "application/json"', function () {
+            res.status.calledWithExactly('Content-Type', 'application/json');
+          });
+        });
+        describe('should call res.json()', function () {
+          it('once', function () {
+            res.json.calledOnce;
+          });
+          it('with the correct error object', function () {
+            res.status.calledWithExactly({
+              message: 'Payload should not be empty',
+            });
+          });
+          it('should not call next()', function () {
+            next.notCalled;
+          });
+        });
       });
     });
   });
